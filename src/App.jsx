3a1e6160116b845a3169ba95cfbed72bb899f2a4
cihankusmus <kusmusc@gmail.com>
2023-09-22 22:16:20 +0300

    sepet e silme fonksiyonu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,129 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import btnImgP from '/add-circle-outline.svg';
-import btnImg from '/vector.svg';
-
-// export function MyComponent() {
-//   // const myMsg = 'Merhabalar';
-//   const myObj = {
-//     msg: 'Selamlar',
-//     cssClass: 'title'
-//   };
-
-//   return(
-//     <>
-//       <h1 className={myObj.cssClass}>{myObj.msg}</h1>
-//     </>
-//   )
-// }
-
-// function Welcome() {
-//   let msg = <h2>lutfen giris yapin</h2>;
-//   let isLoggedIn = false;
-//   if (isLoggedIn) {
-//     msg = <h2>Hos Geldiniz..</h2>;
-//   }
-//   return (
-//     <div>
-//       {msg}
-//     </div>
-//   );
-// }
-// function LogIn() {
-//   return (
-//     <h2>Lutfen Giris Yapin</h2>
-//   )
-// }
-
-// function ProductList() {
-//   const products = [
-//     {
-//       id: 1,
-//       name: 'Elma'
-//     },
-//     {
-//       id: 2,
-//       name: 'Armut'
-//     },
-//     {
-//       id: 3,
-//       name: 'Karpuz'
-//     }
-//   ];
-
-//   const productList = products.map(x => <li key={x.id}>{x.name}</li>);
-
-//   return (
-//     <ul>
-//       {productList}
-//     </ul>
-//   )
-// }
+import { useState } from 'react';
+import { Switch } from 'antd';
+import './App.css';
+import beefp from '../public/beef-pizza.png';
+import ccake from '../public/c-cake.png';
+import icecr from '../public/ice-cream.png';
+import latte from '../public/latte.png';
+import meatv from '../public/meats&veggies.png';
+import pcake from '../public/p-cake.png';
+import stick from '../public/stick-balls.png';
+import sunde from '../public/sundae.png';
+import sushi from '../public/sushi.png';
+import vtaco from '../public/vegetarian-taco.png';
+import sepetIcn from '../public/sepet.svg';
 
 function App() {
   const [cart, setCart] = useState([]);
   const [count, setCount] = useState(0);
+  const [isSwitchChecked, setIsSwitchChecked] = useState(false);
 
-  function addToCart(productName, price) {
+  const productscollation = [
+    {
+      id: 1,
+      name: 'Beef pizza with veggies',
+      price: '10',
+      img: beefp
+    },
+    {
+      id: 2,
+      name: 'Meats and veggies',
+      price: '15',
+      img: meatv
+    },
+    {
+      id: 3,
+      name: 'Vegetarian taco-zucchini',
+      price: '12',
+      img: vtaco
+    },
+    {
+      id: 4,
+      name: 'Stick balls',
+      price: '8',
+      img: stick
+    },
+    {
+      id: 5,
+      name: 'Cold sushi',
+      price: '18',
+      img: sushi
+    }
+  ];
+
+  const productsDessert = [
+    {
+      id: 1,
+      name: 'Strawberry pink cake',
+      price: '9',
+      img: pcake
+    },
+    {
+      id: 2,
+      name: 'Crisp chocolate cake',
+      price: '12',
+      img: ccake
+    },
+    {
+      id: 3,
+      name: 'Chocolate ice cream latte',
+      price: '7',
+      img: latte
+    },
+    {
+      id: 4,
+      name: 'Ice cream sundae',
+      price: '13',
+      img: sunde
+    },
+    {
+      id: 5,
+      name: 'Strawberry ice cream',
+      price: '5',
+      img: icecr
+    }
+  ];
+
+  const addToCart = (productName, price) => {
     const newCart = [...cart, { name: productName, price }];
     setCart(newCart);
     setCount(count + price);
-  }
+  };
 
-  function clearCart() {
+  const clearCart = () => {
     setCart([]);
     setCount(0);
-  }
+  };
+  const removeFromCart = (productName, productPrice) => {
+    const updatedCart = cart.filter(item => item.name !== productName);
+    setCart(updatedCart);
+    setCount(count - parseFloat(productPrice));
+  };
 
-  
+  const Duzenle = () => {
+    const duzenleBtn = document.getElementById('duzenleBtn');
 
-  function handleCategoryChange(event) {
+    const onChange = (checked) => {
+      setIsSwitchChecked(checked);
+
+      if (checked) {
+        // Switch açıldığında "Düzenle" butonunu göster
+        duzenleBtn.style.display = 'block';
+      } else {
+        // Switch kapandığında "Düzenle" butonunu gizle
+        duzenleBtn.style.display = 'none';
+      }
+    };
+
+    return <Switch defaultChecked={isSwitchChecked} onChange={onChange} />;
+  };
+
+  const handleCategoryChange = (event) => {
     const selectedCategory = event.target.value;
-  
+
     const aparatifContent = document.getElementById('aparatifContent');
     const dessertContent = document.getElementById('dessertContent');
-  
+
     if (selectedCategory === 'all') {
       aparatifContent.style.display = 'block';
       dessertContent.style.display = 'block';
@@ -95,13 +134,39 @@ function App() {
       aparatifContent.style.display = 'none';
       dessertContent.style.display = 'block';
     }
-  }
-  
+  };
+
+  const productListDessert = productsDessert.map((product) => (
+    <div key={product.id} className="product">
+      {product.img && <img src={product.img} alt={product.name} />}
+      <p>{product.name}</p>
+      <p className="price">${product.price}</p>
+      <button
+        className="productBtn"
+        onClick={() => addToCart(product.name, parseFloat(product.price))}
+      >
+        Sepete Ekle
+      </button>
+    </div>
+  ));
+
+  const productList = productscollation.map((product) => (
+    <div key={product.id} className="product">
+      {product.img && <img src={product.img} alt={product.name} />}
+      <p>{product.name}</p>
+      <p className="price">${product.price}</p>
+      <button
+        className="productBtn"
+        onClick={() => addToCart(product.name, parseFloat(product.price))}
+      >
+        Sepete Ekle
+      </button>
+    </div>
+  ));
 
   return (
     <div>
       <h1>Aperatif ve Tatlı Kategorileri</h1>
-
       {/* Kategori seçim */}
       <label htmlFor="category">Kategori Seç:</label>
       <select id="category" onChange={handleCategoryChange}>
@@ -111,93 +176,25 @@ function App() {
       </select>
       {/* Ürünler */}
       <div id="aparatifContent" className="content">
-          <h2>Aperatif</h2>
-      <div className="product">
-        <img src="public/beef-pizza.png" alt="Beef pizza with veggies" />
-        <p>Beef pizza with veggies </p><p className='price'>$10</p>
-        <button className="productBtn" onClick={() => addToCart('Beef pizza', 10)}>
-          Sepete Ekle
-        </button>
+      <h2>Aperatif</h2>
+        {productList}
       </div>
-      <div className="product">
-        <img src="public/meats&veggies.png" alt="Meats and veggies" />
-        <p>Meats and veggies</p><p className='price'>$15</p>
-        <button className="productBtn" onClick={() => addToCart('Meats and veggies', 15)}>
-          Sepete Ekle
-        </button>
+      <div id="dessertContent" className="content">
+      <h2>Tatlı</h2>
+        {productListDessert}
       </div>
 
-      <div className="product">
-        <img src="public/vegetarian-taco.png" alt="Vegetarian taco-zucchini" />
-        <p>Vegetarian taco-zucchini</p><p className='price'>$12</p>
-        <button className="productBtn" onClick={() => addToCart('Vegetarian taco-zucchini', 12)}>
-          Sepete Ekle
-        </button>
-      </div>
-
-      <div className="product">
-        <img src="public/stick-balls.png" alt="Stick balls" />
-        <p>Stick balls</p><p className='price'>$8</p>
-        <button className="productBtn" onClick={() => addToCart('Stick balls', 8)}>
-          Sepete Ekle
-        </button>
-      </div>
-
-      <div className="product">
-        <img src="public/sushi.png" alt="Cold sushi" />
-        <p>Cold sushi</p><p className='price'>$18</p>
-        <button className="productBtn" onClick={() => addToCart('Cold sushi', 18)}>
-          Sepete Ekle
-        </button>
-      </div> 
-      </div>
-      <div id="dessertContent" class="content">
-          <h2>Tatlı</h2>
-          <div class="products yemek">
-              <div class="product">
-                  <img src="public/p-cake.png" alt=""/>
-                  <p>Straberry pink cake</p><p className='price'>$9</p>
-                  <button className="productBtn" onClick={() => addToCart('Straberry pink cake', 9)}>
-          Sepete Ekle
-        </button>
-              </div>
-              <div class="product">
-                  <img src="public/c-cake.png" alt=""/>
-                  <p>Crisp chocolate cake</p><p className='price'>$12</p>
-                  <button className="productBtn" onClick={() => addToCart('Crisp chocolate cake', 12)}>
-          Sepete Ekle
-        </button>
-              </div>
-              <div class="product">
-                  <img src="public/latte.png" alt=""/>
-                  <p>Chocolate ice cream latte</p><p className='price'>$7</p>
-                  <button className="productBtn" onClick={() => addToCart('Chocolate ice cream latte', 7)}>
-          Sepete Ekle
-        </button>
-              </div>
-              <div class="product">
-                  <img src="public/sundae.png" alt=""/>
-                  <p>Ice cream sundae</p><p className='price'>$13</p>
-                  <button className="productBtn" onClick={() => addToCart('Ice cream sundae', 13)}>
-          Sepete Ekle
-        </button>
-              </div>
-              <div class="product">
-                  <img src="public/ice-cream.png" alt=""/>
-                  <p>Strawberry ice cream</p><p className='price'>$5</p>
-                  <button className="productBtn" onClick={() => addToCart('Strawberry ice cream', 5)}>
-          Sepete Ekle
-        </button>
-              </div>
-          </div>      
-        </div>      
-
-      
       <div className="cart">
+        <img src={sepetIcn} alt="" />
         <h2>Sepet</h2>
-        <ul className='cartItem'>
+        {Duzenle()}
+        <ul className="cartItem">
           {cart.map((item, index) => (
             <li key={index}>
+              <button
+              className="duzenleBtn"
+              onClick={() => removeFromCart(item.name, item.price)}
+            >-</button>
               {item.name}: ${item.price}
             </li>
           ))}
@@ -206,9 +203,7 @@ function App() {
         <button onClick={clearCart}>Sepeti Temizle</button>
       </div>
     </div>
-    
   );
 }
 
 export default App;
-
